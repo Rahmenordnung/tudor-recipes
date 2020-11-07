d0ade00a1b2482a66f60fd4bb08325276fc50d84
EC2 Default User <ec2-user@ip-172-31-34-101.ec2.internal>
2020-11-07 23:53:23 +0000

    add ingredient model and many to many table assotionations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201107155016) do
+ActiveRecord::Schema.define(version: 20201107234316) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20201107155016) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
