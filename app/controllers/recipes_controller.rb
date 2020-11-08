2eaa6459eb3763a06a0537a1694772f696f62f4e
EC2 Default User <ec2-user@ip-172-31-34-101.ec2.internal>
2020-11-08 17:53:49 +0000

    complete ingredients feature and relations with chefs and recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,7 +52,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :cooking_time)
+    params.require(:recipe).permit(:name, :description, :cooking_time, ingredient_ids: [])
   end
   
     def require_same_user
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  resources :ingredients, except: [:destroy]
 end
 
 
