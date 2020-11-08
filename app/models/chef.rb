3a36ec5f4e6e8f1b9ffef8279e14249a4038ed67
EC2 Default User <ec2-user@ip-172-31-34-101.ec2.internal>
2020-11-08 17:01:59 +0000

    add commennts models

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,5 +9,6 @@ class Chef < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_secure_password
   validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+  has_many :comments, dependent: :destroy
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,9 @@ class Recipe < ApplicationRecord
   validates :cooking_time, presence: true
   belongs_to :chef
   validates :chef_id, presence: true
+  #latest recipe is going to be displayed the first one
   default_scope -> { order(updated_at: :desc)} 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+  has_many :comments, dependent: :destroy
 end
