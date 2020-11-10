a9324c2fed086132cd61b9fb8ae57b0607f34500
EC2 Default User <ec2-user@ip-172-31-34-101.ec2.internal>
2020-11-10 20:26:11 +0000

    add search functionality working and layout

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,7 @@
 class SearchController < ApplicationController
   def search
      st = "%#{params[:q]}%"
-     @recipes = Recipe.where("name like ?", st)
-  end
-  
-#   def search
-    # st = "%#{params[:q]}%"
-    # @items = Item.where("title like ? or description like ? or category like ? or provenience like ? or price like ? " , st, st, st, st, st).paginate(page: params[:page], per_page: 10)
-# end
 
+      @recipes = Recipe.where("name like ? or description like ? or cooking_time like ? " , st, st, st ).paginate(page: params[:page], per_page: 10) 
+  end
 end
