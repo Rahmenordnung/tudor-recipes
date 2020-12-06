55be5b43ceada2cfcc2b98d8d4b79b63d28b72a2
EC2 Default User <ec2-user@ip-172-31-36-242.ec2.internal>
2020-12-06 22:22:58 +0000

    add sendgrid production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://yummy-e-recipes.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://yummy-e-recipes.herokuapp.com', 'http://yummy-e-recipes.herokuapp.com']
+  
+  config.action_mailer.default_url_options = {host: 'https://yummy-e-recipes.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
+
 end
