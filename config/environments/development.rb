d4ea8da52fed930668e569573e0b3d5c31e157d6
EC2 Default User <ec2-user@ip-172-31-36-242.ec2.internal>
2020-12-08 00:48:55 +0000

    added text to html pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,15 +55,5 @@ Rails.application.configure do
   
   
   # For mail_form gem
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors= true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                      587,
-    domain:                 'gmail.com',
-    user_name:             'tudorpascu787@gmail.com',
-    password:              'Nurflussigkeiten1@',
-    authentication:      'plain',
-    enable_starttls_auto: true  }
+
 end
